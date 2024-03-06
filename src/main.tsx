c965c6ad4615d4fe26a56f684a8e7a72f6a909b1
matthieuGravy <matthieugravy@gmail.com>
2024-03-06 10:26:11 +0100

    modification architecture

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,11 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-//import { Provider } from "react-redux";
-//import { PersistGate } from "redux-persist/integration/react";
 
 import { router } from "./router.jsx";
-//import { store, persistor } from "./store/";
+
 
 import App from "./App.tsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 import "./index.css";
 
@@ -25,10 +16,5 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
-
-    {/*<Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>  
-      </PersistGate>
-    </Provider>*/}
   </StrictMode>
 );
