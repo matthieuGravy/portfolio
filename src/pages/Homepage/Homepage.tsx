238b6f18b46ca8a00f2f134553f7ff68ebe1b008
matthieuGravy <matthieugravy@gmail.com>
2024-05-25 20:38:05 +0200

    mobile home

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,5 +1,5 @@
-import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
+import { motion } from "framer-motion";
+
 import { GrGithub, GrLinkedinOption } from "react-icons/gr";
 
 import MatthieuGravyDeveloperWeb from "../../assets/pictures/matthieu-gravy-full-background-colors.svg";
@@ -46,140 +46,146 @@ const Homepage = () => {
   ));
   return (
     <>
-      <Containers
-        type="section-large"
-        className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-8 rounded-xl lg:gap-y-16 xl:gap-y-0 gap-x-16 relative"
-      >
-        <article className={`${styleRow}`}>
-          <Heading title="Web developer" level="h1" />
-          <section className="relative">
-            <Heading title={titleAnimation} level="h3" className="" />
-          </section>
-          <section className="flex gap-x-8 justify-center">
-            <Buttonext
-              link="https://www.linkedin.com/in/matthieuGravy"
-              title="Linkedin"
-              className="flex flex-row justify-start rounded-lg"
-              content={
-                <>
-                  <motion.figure
-                    className="hover:shadow-2xl transition-all duration-500 hover:bg-teal-200 rounded-full p-2 "
-                    initial={({ y: 0 }, { opacity: 0 })}
-                    animate={({ y: 16 }, { opacity: 1 })}
-                    transition={{
-                      duration: 1,
-                      delay: 3,
-                      repeatType: "reverse",
-                    }}
-                  >
-                    <GrLinkedinOption
-                      size={24}
-                      color="#3f3f46"
-                      className={classname}
-                    />
-                  </motion.figure>
-                </>
-              }
+      <main className="overflow-hidden">
+        <Containers
+          type="section-large"
+          className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-8 rounded-xl gap-y-24 lg:gap-y-16 xl:gap-y-0 gap-x-16 relative"
+        >
+          <article className={`${styleRow}`}>
+            <Heading title="Web developer" level="h1" />
+            <section className="relative">
+              <Heading title={titleAnimation} level="h3" className="" />
+            </section>
+            <section className="flex gap-x-8 justify-center">
+              <Buttonext
+                link="https://www.linkedin.com/in/matthieuGravy"
+                title="Linkedin"
+                className="flex flex-row justify-start rounded-lg"
+                content={
+                  <>
+                    <motion.figure
+                      className="hover:shadow-2xl transition-all duration-500 hover:bg-teal-200 rounded-full p-2 "
+                      initial={({ y: 0 }, { opacity: 0 })}
+                      animate={({ y: 16 }, { opacity: 1 })}
+                      transition={{
+                        duration: 1,
+                        delay: 3,
+                        repeatType: "reverse",
+                      }}
+                    >
+                      <GrLinkedinOption
+                        size={24}
+                        color="#3f3f46"
+                        className={classname}
+                      />
+                    </motion.figure>
+                  </>
+                }
+              />
+              <Buttonext
+                link="https://github.com/matthieuGravy"
+                title="Github"
+                className="flex flex-row items-center"
+                content={
+                  <>
+                    <motion.figure
+                      className="hover:shadow-2xl transition-all duration-500 hover:bg-teal-200 rounded-full p-2"
+                      initial={({ y: 0 }, { opacity: 0 })}
+                      animate={({ y: 16 }, { opacity: 1 })}
+                      transition={{
+                        duration: 1,
+                        delay: 3.5,
+                        repeatType: "reverse",
+                      }}
+                    >
+                      <GrGithub
+                        size={24}
+                        color="#3f3f46"
+                        className={classname}
+                      />
+                    </motion.figure>
+                  </>
+                }
+              />
+            </section>
+          </article>
+          <header className={styleRow}>
+            <Heading title={greeting} level="h4" className={``} />
+            <ScrollHeading level="h3" className="" title="Matthieu Gravy" />
+          </header>
+
+          <figure
+            className={`lg:row-start-2 lg:row-span-2 lg:col-start-2 z-20`}
+          >
+            <img
+              src={matthieuFocus}
+              alt="Illustration"
+              className="max-h-900px w-4/5 mx-auto rounded-xl"
             />
-            <Buttonext
-              link="https://github.com/matthieuGravy"
-              title="Github"
-              className="flex flex-row items-center"
-              content={
-                <>
-                  <motion.figure
-                    className="hover:shadow-2xl transition-all duration-500 hover:bg-teal-200 rounded-full p-2"
-                    initial={({ y: 0 }, { opacity: 0 })}
-                    animate={({ y: 16 }, { opacity: 1 })}
-                    transition={{
-                      duration: 1,
-                      delay: 3.5,
-                      repeatType: "reverse",
-                    }}
-                  >
-                    <GrGithub size={24} color="#3f3f46" className={classname} />
-                  </motion.figure>
-                </>
-              }
+          </figure>
+          <ScrollTextRight className={styleRow}>
+            <p className={styleP}>
+              A web developer based in Brussels, specializing in modern web
+              technologies. I am capable of creating web projects quickly and
+              easily.
+            </p>
+            <section className="">
+              <ButtonOne content="more" to="/about" className="" />{" "}
+            </section>
+          </ScrollTextRight>
+          <figure className={`flex items-center row-span-2 `}>
+            <Scroller />
+          </figure>
+
+          <article className={styleRow}>
+            <ScrollHeading
+              level="h2"
+              className="md:w-[400px]"
+              title="My Skills"
             />
-          </section>
-        </article>
-        <header className={styleRow}>
-          <Heading title={greeting} level="h4" className={``} />
-          <ScrollHeading level="h3" className="" title="Matthieu Gravy" />
-        </header>
+          </article>
+          <ScrollTextLeft className={styleRow}>
+            <p className={styleP}>
+              I specialize in React, Javascript, and Tailwind CSS. I also have
+              experience with Typescript, Vue.js, Node.js, Express, and MongoDB.
+            </p>
+          </ScrollTextLeft>
+        </Containers>
 
-        <figure className={`lg:row-start-2 lg:row-span-2 lg:col-start-2 z-20`}>
-          <img
-            src={matthieuFocus}
-            alt="Illustration"
-            className="max-h-900px w-4/5 mx-auto rounded-xl"
-          />
-        </figure>
-        <ScrollTextRight className={styleRow}>
-          <p className={styleP}>
-            A web developer based in Brussels, specializing in modern web
-            technologies. I am capable of creating web projects quickly and
-            easily.
-          </p>
-          <section className="">
-            <ButtonOne content="more" to="/about" className="" />{" "}
-          </section>
-        </ScrollTextRight>
-        <figure className={`flex items-center row-span-2 `}>
-          <Scroller />
+        <figure className="py-24 lg:py-16 xl:py-0">
+          <img src={triangle} alt="" className="w-full" />
+          <ParallaxTextor velocity={1} />
+          <img src={triangleFlip} alt="" className="w-full" />
         </figure>
 
-        <article className={styleRow}>
-          <ScrollHeading
-            level="h2"
-            className="md:w-[400px]"
-            title="My Skills"
-          />
-        </article>
-        <ScrollTextLeft className={styleRow}>
-          <p className={styleP}>
-            I specialize in React, Javascript, and Tailwind CSS. I also have
-            experience with Typescript, Vue.js, Node.js, Express, and MongoDB.
-          </p>
-        </ScrollTextLeft>
-      </Containers>
+        <Containers
+          type="section-large"
+          className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-4 rounded-xl gap-y-24 lg:gap-y-16 xl:gap-y-0 gap-x-16 "
+        >
+          <figure className="rounded-xl p-4 lg:row-span-2 lg:row-start-2 lg:col-start-2">
+            <motion.img
+              src={MatthieuGravyDeveloperWeb}
+              alt="Matthieu Gravy web developer illustration"
+              className="max-h-900px w-4/5 mx-auto rounded-xl"
+            />
+          </figure>
+          <article
+            className={`lg:col-start-1 lg:row-span-2 bg-red-300 ${styleRow}`}
+          >
+            <ScrollHeading level="h2" className="" title="My projects" />
+          </article>
 
-      <figure className="">
-        <img src={triangle} alt="" className="w-full" />
-        <ParallaxTextor velocity={1} />
-        <img src={triangleFlip} alt="" className="w-full" />
-      </figure>
-
-      <Containers
-        type="section-large"
-        className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-4 rounded-xl lg:gap-y-16 xl:gap-y-0 gap-x-16 "
-      >
-        <figure className="rounded-xl p-4 row-span-2 row-start-2 col-start-2">
-          <motion.img
-            src={MatthieuGravyDeveloperWeb}
-            alt="Matthieu Gravy web developer illustration"
-            className="max-h-900px w-4/5 mx-auto rounded-xl"
-          />
-        </figure>
-        <article className={`lg:col-start-1 row-span-2 ${styleRow}`}>
-          <ScrollHeading
-            level="h2"
-            className="md:w-[400px]"
-            title="My projects"
-          />
-        </article>
-
-        <ScrollTextLeft className={`${styleRow}`}>
-          <p className={styleP}>
-            These projects are initially test projects to help me understand
-            certain technologies. They are currently under review to propose
-            something more elegant.
-          </p>
-          <ButtonOne content="Discover" to="/projects" />
-        </ScrollTextLeft>
-      </Containers>
-      <Getintouch />
+          <ScrollTextLeft className={`${styleRow} bg-red-200`}>
+            <p className={styleP}>
+              These projects are initially test projects to help me understand
+              certain technologies. They are currently under review to propose
+              something more elegant.
+            </p>
+            <ButtonOne content="Discover" to="/projects" />
+          </ScrollTextLeft>
+        </Containers>
+        <Getintouch />
+      </main>
     </>
   );
 };
--- src/pages/Projetspage/childrens/Projectpage/Projectpage.tsx
+++ src/pages/Projetspage/childrens/Projectpage/Projectpage.tsx
@@ -7,37 +7,80 @@ const Projectpage = () => {
   return (
     <>
       <Containers type="section-large" id="projects" className="">
-        <article className="bg-red-200 relative grid grid-cols-4 grid-rows-4">
-          <div className="cols-span-2 row-span-2 col-start-2 row-start-2 bg-yellow-200 w-[700px] h-[700px] rounded-[40vh] flex justify-center items-center">
-            {" "}
-            <Heading
-              level="h2"
-              className="text-center md:text-start"
-              title={
-                <>
-                  Learning
-                  <br /> Projects
-                </>
-              }
-            />
+        <div>
+          {" "}
+          <Heading
+            level="h2"
+            className=""
+            title={
+              <>
+                Learning
+                <br /> Projects
+              </>
+            }
+          />
+          <p>
+            J'ai participé à 5 projets qui m'ont permis d'acquérir une
+            expérience significative sur la gestion de projet, et le
+            développement web.
+          </p>
+          <h2>Its Gravy Kitchen,</h2>
+          <p>
+            C'est un projet html/bootstrap qui m'a rendu fou. Avant de commencer
+            le bootcamp avec Becode j'avais déjà appris bootstrap, sass,
+            JavaScript, Vue3. On ne pouvait pas utiliser de framework ou
+            JavaScript ce qui a rendu les taches répétitives et longues. Pour un
+            résultat moyen.
+          </p>
+          <h2>Plant Clicker</h2>
+          <p>
+            C'est un jeu au click, l'idée sur ce projet c'était de se départager
+            les tâches sur un projet commun. La difficulté que j'ai rencontré
+            c'était de comprendre ce que faisait les autres quand il codait.
+          </p>
+          <p>
+            Mais, je trouvais que dans ce projet on manquait de communication.
+            Donc j'ai décidé d'utiliser l'observeur de javascript pour arriver à
+            l'objectif quand je devais faire le panel de scores.
+          </p>
+          <h2>Sora Cine</h2>
+          <p>
+            C'était un projet avec React en équipe de 2 front 2 back. Les
+            back-end avaient du mal à démarrer sur nodejs, à intégrer une API,
+            créer des schemas mongoose. J'ai donc pris le lead sur le front-end
+            pour les aider à démarrer. Mais le projet n'a pas été terminé.
+          </p>
+          <h2>Social Network</h2>
+          <p>
+            J'étais pas super content d'avor eu une mauvaise expérience en
+            back-end, et je suis un touche à tout. Donc je me suis lancé comme
+            back-end pour ce projet. J'ai appris à utiliser express, mongoose,
+            et à créer des routes.
+          </p>
+          <p>
+            Le back-end est fonctionnel, mais le front-end n'a jamais réellement
+            débuté.
+          </p>
+        </div>
+        <article className="bg-red-200 grid grid-cols-4 grid-rows-4">
+          <div>
+            {projet.map((projet) => (
+              <CardProject
+                key={projet.id}
+                id={projet.id}
+                to={projet.to}
+                name={projet.name}
+                liste={projet.tech.map((tech) => (
+                  <li
+                    key={tech}
+                    className="border-[1px] border-zinc-200 px-4 py-2 uppercase text-xs "
+                  >
+                    {tech}e
+                  </li>
+                ))}
+              />
+            ))}
           </div>
-
-          {projet.map((projet) => (
-            <CardProject
-              key={projet.id}
-              id={projet.id}
-              to={projet.to}
-              name={projet.name}
-              liste={projet.tech.map((tech) => (
-                <li
-                  key={tech}
-                  className="border-[1px] border-zinc-200 px-4 py-2 uppercase text-xs "
-                >
-                  {tech}e
-                </li>
-              ))}
-            />
-          ))}
         </article>
       </Containers>
     </>
