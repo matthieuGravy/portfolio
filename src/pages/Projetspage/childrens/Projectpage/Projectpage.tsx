3af5aa6585c7ffbcd909cf57c8dbda420b7f49c9
matthieuGravy <matthieugravy@gmail.com>
2024-06-04 13:57:44 +0200

    projects responsive

--- src/pages/Projetspage/childrens/Projectpage/Projectpage.tsx
+++ src/pages/Projetspage/childrens/Projectpage/Projectpage.tsx
@@ -14,14 +14,14 @@ const Projectpage = () => {
       <Containers
         type="section-large"
         id="projects"
-        className="py-24 flex flex-col gap-24"
+        className="py-24 flex flex-col"
       >
         <article>
-          <Heading title="Projects" level="h2" className="" />
+          <Heading title="Projects" level="h2" className="pb-16 md:pb-0" />
         </article>
-        <article className="grid grid-cols-3 gap-8">
-          <header className="col-span-3 grid grid-cols-2 grid-rows-4 gap-y-24 lg:gap-y-16 xl:gap-y-0 gap-x-16 ">
-            <ScrollTextLeft className={`row-span-2 row-start-2`}>
+        <article className="">
+          <header className=" grid md:grid-cols-2 md:grid-rows-4 gap-y-12 md:gap-y-0 gap-x-16 pb-24 md:pb-0 ">
+            <ScrollTextLeft className={`md:row-span-2 md:row-start-2`}>
               <p className={`  ${styleP}`}>
                 I participated in a bootcamp with BeCode. I had already started
                 developing on my own, but I needed more structure and guidance.
@@ -32,10 +32,10 @@ const Projectpage = () => {
             </ScrollTextLeft>
             <ScrollHeading
               level="h4"
-              className="row-span-2 "
+              className="md:row-span-2 md:self-center"
               title="Learnings"
             />{" "}
-            <ScrollTextRight className={`row-span-2 `}>
+            <ScrollTextRight className={`md:row-span-2 `}>
               <p className={`${styleP}`}>
                 All these projects may not be visually or technically amazing,
                 but they taught me a lot in their context. I am still happy to
@@ -44,48 +44,48 @@ const Projectpage = () => {
               </p>
             </ScrollTextRight>
           </header>
-          {learnings.map((projet) => (
-            <CardProject
-              key={projet.id}
-              id={projet.id}
-              to={projet.to}
-              name={projet.name}
-              liste={projet.tech.map((tech) => (
-                <li
-                  key={tech}
-                  className="border-[1px] border-zinc-200 px-4 py-2 uppercase text-xs "
-                >
-                  {tech}
-                </li>
-              ))}
-            />
-          ))}
+          <section className="grid md:grid-cols-2 xl:grid-cols-3">
+            {learnings.map((projet) => (
+              <CardProject
+                key={projet.id}
+                id={projet.id}
+                to={projet.to}
+                name={projet.name}
+                liste={projet.tech.map((tech) => (
+                  <li
+                    key={tech}
+                    className="border-[1px] border-zinc-200 px-4 py-2 uppercase text-xs "
+                  >
+                    {tech}
+                  </li>
+                ))}
+              />
+            ))}
+          </section>
         </article>
 
-        <article className="grid grid-cols-3 gap-8">
-          <header className="col-span-3 grid grid-cols-2 grid-rows-4 gap-y-24 lg:gap-y-16 xl:gap-y-0 gap-x-16 ">
-            <ScrollHeading
-              level="h4"
-              className="col-start-2"
-              title="Professional"
-            />
+        <article>
+          <header className="pt-24 pb-12 ">
+            <ScrollHeading level="h4" className="" title="Professional" />
           </header>
-          {professionals.map((projet) => (
-            <CardProject
-              key={projet.id}
-              id={projet.id}
-              to={projet.to}
-              name={projet.name}
-              liste={projet.tech.map((tech) => (
-                <li
-                  key={tech}
-                  className="border-[1px] border-zinc-200 px-4 py-2 uppercase text-xs "
-                >
-                  {tech}
-                </li>
-              ))}
-            />
-          ))}
+          <section className="grid md:grid-cols-2 xl:grid-cols-3">
+            {professionals.map((projet) => (
+              <CardProject
+                key={projet.id}
+                id={projet.id}
+                to={projet.to}
+                name={projet.name}
+                liste={projet.tech.map((tech) => (
+                  <li
+                    key={tech}
+                    className="border-[1px] border-zinc-200 px-4 py-2 uppercase text-xs "
+                  >
+                    {tech}
+                  </li>
+                ))}
+              />
+            ))}
+          </section>
         </article>
       </Containers>
     </>
