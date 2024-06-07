fbc67d2b5f4ae3185f813f9e5d54bb6fb2bbc7cd
matthieuGravy <matthieugravy@gmail.com>
2024-06-08 00:13:06 +0200

    get in touch

--- src/components/Getintouch/Getintouch.tsx
+++ src/components/Getintouch/Getintouch.tsx
@@ -5,7 +5,7 @@ const Getintouch = () => {
   return (
     <>
       <img src={triangle} alt="" className="w-full" />
-      <div className="w-full bg-yellow-200 flex justify-center">
+      <div className="w-full bg-yellow-200 flex justify-center py-24 md:py-0">
         <ContactForm />
       </div>
     </>
