0b44bc24d212896969eafee87e7fb6a952e2c175
matthieuGravy <matthieugravy@gmail.com>
2024-06-10 10:56:54 +0200

    dev

--- src/pages/Contactpage/Contactpage.tsx
+++ src/pages/Contactpage/Contactpage.tsx
@@ -4,6 +4,8 @@ import * as Yup from "yup";
 import { useState } from "react";
 
 import Paragraphe from "../../components/blocs/Paragraphe";
+import Heading from "../../components/blocs/Heading";
+import Containers from "../../components/Containers";
 
 interface MyFormValues {
   prenom: string;
@@ -16,6 +18,9 @@ interface MyFormValues {
 
 const Contactpage = () => {
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [building] = useState(true);
+
+  const styleP = "text-2xl tracking-widest font-sintony ";
   /*
   const handleRecaptcha = (value) => {
     console.log(value);
@@ -91,163 +96,181 @@ const Contactpage = () => {
   };
   return (
     <>
-      <>
-        <Paragraphe children="Vous avez une question, un projet ou une idée ? N'hésitez pas à me contacter via le formulaire ci-dessous. Je vous répondrai dans les plus brefs délais." />
-      </>
-
-      {formSubmitted ? (
+      {building ? (
         <>
-          <Paragraphe
-            className="text-center py-24"
-            children={
-              <>
-                <>Votre message a bien été envoyé.</>
-                <br /> Merci !
-              </>
-            }
-          />
+          <Containers
+            type="section-large"
+            className="min-h-screen flex flex-col gap-y-12 pt-36"
+          >
+            <Heading
+              title="contact"
+              level="h2"
+              className="text-center md:text-start"
+            />
+            <p className={styleP}>This page is under development.</p>
+          </Containers>
         </>
       ) : (
         <>
-          {/*<ReCAPTCHA sitekey="votre-clé-de-site" onChange={handleRecaptcha}> */}
-          <Formik
-            initialValues={{
-              nom: "",
-              prenom: "",
-              email: "",
-              entreprise: "",
-              sujet: "",
-              message: "",
-            }}
-            validationSchema={SignupSchema}
-            onSubmit={submitForm}
-          >
-            {({ errors, touched, values }) => (
-              <Form className="md:w-3/5 lg:w-2/5 mx-auto py-24">
-                <section className="grid md:grid-cols-2 grid-rows-3 gap-y-8 md:gap-x-12 py-8">
-                  <label className="relative">
-                    <Field
-                      name="nom"
-                      placeholder="Nom"
-                      className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
-                        errors.nom && touched.nom
-                          ? "border-red-500"
-                          : values.nom === ""
-                          ? "border-natural-50"
-                          : "border-green-500"
-                      }`}
-                    />
-                    {errors.nom && touched.nom ? (
-                      <p className="absolute text-red-500 left-1">
-                        {errors.nom}
-                      </p>
-                    ) : null}
-                  </label>
-                  <label className="relative md:place-self-end md:w-full">
-                    <Field
-                      name="prenom"
-                      placeholder="Prénom"
-                      className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full  ${
-                        errors.prenom && touched.prenom
-                          ? "border-red-500"
-                          : values.prenom === ""
-                          ? "border-natural-50"
-                          : "border-green-500"
-                      }`}
-                    />
-                    {errors.prenom && touched.prenom ? (
-                      <p className="absolute text-red-500 left-1">
-                        {errors.prenom}
-                      </p>
-                    ) : null}
-                  </label>
-                  <label className="relative">
-                    <Field
-                      name="email"
-                      type="email"
-                      placeholder="Email"
-                      className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
-                        errors.email && touched.email
-                          ? "border-red-500"
-                          : values.email === ""
-                          ? "border-natural-50"
-                          : "border-green-500"
-                      }`}
-                    />
-                    {errors.email && touched.email ? (
-                      <p className="absolute text-red-500 left-1">
-                        {errors.email}
-                      </p>
-                    ) : null}
-                  </label>
-                  <label className="relative md:place-self-end md:w-full">
-                    <Field
-                      name="entreprise"
-                      placeholder="Entreprise"
-                      className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
-                        errors.entreprise && touched.entreprise
-                          ? "border-red-500"
-                          : values.entreprise === ""
-                          ? "border-natural-50"
-                          : "border-green-500"
-                      }`}
-                    />
-                    {errors.entreprise && touched.entreprise ? (
-                      <p className="absolute text-red-500 left-1">
-                        {errors.entreprise}
-                      </p>
-                    ) : null}
-                  </label>
-                  <label className="relative">
-                    <Field
-                      name="sujet"
-                      placeholder="Sujet"
-                      className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
-                        errors.sujet && touched.sujet
-                          ? "border-red-500"
-                          : values.sujet === ""
-                          ? "border-natural-50"
-                          : "border-green-500"
-                      }`}
-                    />
-                    {errors.sujet && touched.sujet ? (
-                      <p className="absolute text-red-500 left-1">
-                        {errors.sujet}
-                      </p>
-                    ) : null}
-                  </label>
-                </section>
-                <section className="flex flex-col gap-y-8">
-                  <label className="relative">
-                    <Field
-                      as="textarea"
-                      name="message"
-                      placeholder="Message"
-                      className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none w-full ${
-                        errors.message && touched.message
-                          ? "border-red-500"
-                          : values.message === ""
-                          ? "border-natural-50"
-                          : "border-green-500"
-                      }`}
-                    />
-                    {errors.message && touched.message ? (
-                      <p className="absolute text-red-500 left-1">
-                        {errors.message}
-                      </p>
-                    ) : null}
-                  </label>
-                  <button
-                    type="submit"
-                    className="uppercase w-full bg-neutral-50 text-zinc-800 flex justify-center py-2 hover:bg-fuchsia-700 hover:text-neutral-50 transition-colors"
-                  >
-                    Envoyer
-                  </button>
-                </section>
-              </Form>
-            )}
-          </Formik>
-          {/*</ReCAPTCHA>*/}
+          <>
+            <Paragraphe children="Vous avez une question, un projet ou une idée ? N'hésitez pas à me contacter via le formulaire ci-dessous. Je vous répondrai dans les plus brefs délais." />
+          </>
+
+          {formSubmitted ? (
+            <>
+              <Paragraphe
+                className="text-center py-24"
+                children={
+                  <>
+                    <>Votre message a bien été envoyé.</>
+                    <br /> Merci !
+                  </>
+                }
+              />
+            </>
+          ) : (
+            <>
+              {/*<ReCAPTCHA sitekey="votre-clé-de-site" onChange={handleRecaptcha}> */}
+              <Formik
+                initialValues={{
+                  nom: "",
+                  prenom: "",
+                  email: "",
+                  entreprise: "",
+                  sujet: "",
+                  message: "",
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={submitForm}
+              >
+                {({ errors, touched, values }) => (
+                  <Form className="md:w-3/5 lg:w-2/5 mx-auto py-24">
+                    <section className="grid md:grid-cols-2 grid-rows-3 gap-y-8 md:gap-x-12 py-8">
+                      <label className="relative">
+                        <Field
+                          name="nom"
+                          placeholder="Nom"
+                          className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
+                            errors.nom && touched.nom
+                              ? "border-red-500"
+                              : values.nom === ""
+                              ? "border-natural-50"
+                              : "border-green-500"
+                          }`}
+                        />
+                        {errors.nom && touched.nom ? (
+                          <p className="absolute text-red-500 left-1">
+                            {errors.nom}
+                          </p>
+                        ) : null}
+                      </label>
+                      <label className="relative md:place-self-end md:w-full">
+                        <Field
+                          name="prenom"
+                          placeholder="Prénom"
+                          className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full  ${
+                            errors.prenom && touched.prenom
+                              ? "border-red-500"
+                              : values.prenom === ""
+                              ? "border-natural-50"
+                              : "border-green-500"
+                          }`}
+                        />
+                        {errors.prenom && touched.prenom ? (
+                          <p className="absolute text-red-500 left-1">
+                            {errors.prenom}
+                          </p>
+                        ) : null}
+                      </label>
+                      <label className="relative">
+                        <Field
+                          name="email"
+                          type="email"
+                          placeholder="Email"
+                          className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
+                            errors.email && touched.email
+                              ? "border-red-500"
+                              : values.email === ""
+                              ? "border-natural-50"
+                              : "border-green-500"
+                          }`}
+                        />
+                        {errors.email && touched.email ? (
+                          <p className="absolute text-red-500 left-1">
+                            {errors.email}
+                          </p>
+                        ) : null}
+                      </label>
+                      <label className="relative md:place-self-end md:w-full">
+                        <Field
+                          name="entreprise"
+                          placeholder="Entreprise"
+                          className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
+                            errors.entreprise && touched.entreprise
+                              ? "border-red-500"
+                              : values.entreprise === ""
+                              ? "border-natural-50"
+                              : "border-green-500"
+                          }`}
+                        />
+                        {errors.entreprise && touched.entreprise ? (
+                          <p className="absolute text-red-500 left-1">
+                            {errors.entreprise}
+                          </p>
+                        ) : null}
+                      </label>
+                      <label className="relative">
+                        <Field
+                          name="sujet"
+                          placeholder="Sujet"
+                          className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none md:w-full ${
+                            errors.sujet && touched.sujet
+                              ? "border-red-500"
+                              : values.sujet === ""
+                              ? "border-natural-50"
+                              : "border-green-500"
+                          }`}
+                        />
+                        {errors.sujet && touched.sujet ? (
+                          <p className="absolute text-red-500 left-1">
+                            {errors.sujet}
+                          </p>
+                        ) : null}
+                      </label>
+                    </section>
+                    <section className="flex flex-col gap-y-8">
+                      <label className="relative">
+                        <Field
+                          as="textarea"
+                          name="message"
+                          placeholder="Message"
+                          className={`bg-zinc-800 border-b-[1px] py-2 uppercase focus:outline-none w-full ${
+                            errors.message && touched.message
+                              ? "border-red-500"
+                              : values.message === ""
+                              ? "border-natural-50"
+                              : "border-green-500"
+                          }`}
+                        />
+                        {errors.message && touched.message ? (
+                          <p className="absolute text-red-500 left-1">
+                            {errors.message}
+                          </p>
+                        ) : null}
+                      </label>
+                      <button
+                        type="submit"
+                        className="uppercase w-full bg-neutral-50 text-zinc-800 flex justify-center py-2 hover:bg-fuchsia-700 hover:text-neutral-50 transition-colors"
+                      >
+                        Envoyer
+                      </button>
+                    </section>
+                  </Form>
+                )}
+              </Formik>
+              {/*</ReCAPTCHA>*/}
+            </>
+          )}
         </>
       )}
     </>
