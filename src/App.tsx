042eba164e6eb9bafcd5449297a552c65f7deb3b
matthieuGravy <matthieugravy@gmail.com>
2024-08-01 13:14:56 +0200

    react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Topnav from "./components/Topnav";
 import Footer from "./components/Footer";
+import SEO from "./components/SEO";
 
 function App() {
   return (
     <>
+      <SEO />
       <section className="bg-[#130e11] min-h-screen">
         <Topnav />
         <Outlet />
